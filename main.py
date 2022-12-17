06847a473c1769f43228b73ff25100eea87656f4
Jared-Chapman <jared.chapman.0612@gmail.com>
2022-12-17 12:50:26 -0600

    MVP

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
 import re
+import time
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+sense = SenseHat()
+#sense.low_light=True
 os.system("aconnect -x")
 
 deviceNamesToIgnore = ['MIDI', 'Midi', 'System', 'Timer', 'Announce', 'Through']
@@ -14,11 +18,6 @@ outputString = subprocess.check_output("aconnect -o", shell=True, text=True)
 allInputs =  re.findall(r"'([^']*)'", inputString)
 allOutputs = re.findall(r"'([^']*)'", outputString)
 
-#for x in allInputs[:]:
-    #for y in deviceNamesToIgnore:
-        #if y in x:
-            #allInputs.remove(x)
-            #break
 
 for x in deviceNamesToIgnore:
     for y in allInputs[:]:
@@ -31,6 +30,7 @@ for x in deviceNamesToIgnore:
 print(f"IN: {allInputs}")
 print(f"OUT: {allOutputs}")
 def priorityLink():
+    sense.show_message("Running Priority Link", scroll_speed=.02)
     i = ''
     o = ''
     
@@ -54,4 +54,68 @@ def priorityLink():
         print(f'Connecting {i} to {o}')
         os.system(f"aconnect '{i}' '{o}'")
 
-priorityLink()
+listPosition = 0
+# 0 for selecting input, 1 for selecting output, 2 for sleep
+selecting = 0
+i = ''
+o = ''
+def clamp(value, min_value=0, max_value=len(allInputs)-1):
+    return min(max_value, max(min_value, value))
+
+def pushed_up(event):
+    global listPosition
+    if event.action != ACTION_RELEASED:
+        listPosition = clamp(listPosition - 1)
+        sense.clear()
+
+
+
+def pushed_down(event):
+    global listPosition
+    if event.action != ACTION_RELEASED:
+        listPosition = clamp(listPosition + 1)
+        sense.clear()
+
+def pushed_middle(event):
+    global listPosition
+    global selecting
+    global i
+    global o
+    if event.action != ACTION_RELEASED:
+        print("middle pressed")
+        print(selecting)
+        if selecting == 0:
+            i = allInputs[listPosition]
+            selecting = 1
+        elif selecting == 1:
+            o = allOutputs[listPosition]
+            selecting = 2
+        elif selecting == 2:
+            selecting = 0
+        listPosition = 0
+    sense.clear()
+
+
+sense.stick.direction_up = pushed_up
+sense.stick.direction_down = pushed_down
+sense.stick.direction_middle = pushed_middle
+
+def loop():
+    global i
+    global o
+    global listPosition
+    global selecting
+    while True:
+        while selecting == 0:
+             sense.show_message(allInputs[listPosition], scroll_speed=0.02, text_colour=[0,255,0])
+        while selecting == 1:
+             sense.show_message(allOutputs[listPosition], scroll_speed=0.02, text_colour=[0,0,255])
+        os.system(f"aconnect '{i}' '{o}'")
+        sense.show_message(f"{i} connected to {o}", text_colour=[0,255,255], scroll_speed=0.02)
+        print(f'Connecting {i} to {o}')
+        while selecting == 2:
+
+            #Just wait I guess
+            pass
+
+loop()
